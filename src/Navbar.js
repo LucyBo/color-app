f31e0af0bb53ed9f34ce7c0836d6593768daa3a0
Lucyna Borkowska <lucyna.borkowska@mail.com>
2019-09-22 20:17:25 +0200

    Add withStyles with demo Styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from "react-router-dom";
 import 'rc-slider/assets/index.css';
 import Slider from 'rc-slider';
 import './Navbar.css';
@@ -28,7 +29,9 @@ export default class Navbar extends Component {
     return (
       <header className="Navbar">
         <div className="logo">
-          <a href='#'>reactcolorpicker</a>
+          <Link to="/">
+          reactcolorpicker
+          </Link>
         </div>
         <div className="slider-container">
           <span>Level: {level} </span>
