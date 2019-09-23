f7359b9c73ffbd3c4675cab31d4a8d466436297d
Lucyna Borkowska <lucyna.borkowska@mail.com>
2019-09-23 16:17:04 +0200

    Display colors in SingleColorPalette

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Route, Switch} from "react-router-dom";
 import Palette from "./Palette";
 import PaletteList from "./PaletteList";
+import SingleColorPalette from './SingleColorPalette';
 import seedColors from './seedColors';
 import {generatePalette} from "./colorHelpers";
 
@@ -14,27 +15,38 @@ class App extends Component  {
   render() {
     return (
       <Switch>
-      <Route 
-      exact 
-      path="/" 
-      render={routeProps => <PaletteList 
-      palettes={seedColors} {...routeProps}/>
-      }/>
-      <Route 
-      exact 
-      path="/palette/:id" 
-      render={routeProps => (
-        <Palette 
-        palette={generatePalette(
-          this.findPalette(routeProps.match.params.id)
-        )}
+      <Route
+          exact
+          path='/palette/:paletteId/:colorId'
+          render={() => <SingleColorPalette />}
+          render={routeProps => (
+            <SingleColorPalette
+              colorId={routeProps.match.params.colorId}
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.paletteId)
+              )}
+            />
+          )}
         />
-      )}
-      />
+        <Route
+          exact
+          path='/'
+          render={routeProps => (
+            <PaletteList palettes={seedColors} {...routeProps} />
+          )}
+        />
+        <Route
+          exact
+          path='/palette/:id'
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
       </Switch>
-      // <div>
-      //   <Palette palette={generatePalette(seedColors[4])}/>
-      // </div>
     );
   }
 }
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import './ColorBox.css';
+import {Link} from "react-router-dom";
+import {withRouter} from "react-router-dom";
 
 
 class ColorBox extends Component {
@@ -15,7 +17,7 @@ class ColorBox extends Component {
     });
   }
   render() {
-    const { name, background } = this.props;
+    const { name, background, moreUrl, showLink } = this.props;
     const { copied } = this.state;
     return (
       <CopyToClipboard text={background}>
@@ -33,9 +35,16 @@ class ColorBox extends Component {
             <div className='box-content'>
               <span>{name}</span>
             </div>
+          
             <button className='copy-button'>Copy</button>
           </div>
-          <span className='see-more'>More</span>
+
+          {showLink && (
+          <Link to={moreUrl} 
+          onClick={e =>e.stopPropagation}>
+            <span className='see-more'>More</span>
+          </Link>
+          )}
         </div>
       </CopyToClipboard>
       </CopyToClipboard>
