27afe1d1caa8b93f0a15f0ceeb56215ab79a98d7
Lucyna Borkowska <lucyna.borkowska@mail.com>
2019-10-03 14:00:03 +0200

    Fix Navbar Slider size

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,7 +3,7 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Link } from "react-router-dom";
 import styles from "./styles/ColorBoxStyles";
 import { withStyles } from "@material-ui/styles";
-
+import classNames from "classnames";
 
 class ColorBox extends Component {
   constructor(props) {
@@ -30,8 +30,9 @@ class ColorBox extends Component {
         <div style={{ background }} className={classes.ColorBox}>
           <div
             style={{ background }}
-            className={`${classes.copyOverlay} ${copied &&
-              classes.showOverlay}`}
+            className={classNames(classes.copyOverlay, {
+              [classes.showOverlay]: copied
+            })}
           />
           <div
             className={`${classes.copyMessage} ${copied &&
