d29265eb8d4c38da767107a24638c3460e23c8e0
Lucyna Borkowska <lucyna.borkowska@mail.com>
2020-02-05 09:37:04 +0100

    Updates

---
+++ precache-manifest.9ac8a59958aaf41917e83e0b4ccf411c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475c0cb2b5111efae66526bc2d5a7c8a",
+    "url": "/color-app/index.html"
+  },
+  {
+    "revision": "442fe8de46dd5ac53fb9",
+    "url": "/color-app/static/css/2.292c8731.chunk.css"
+  },
+  {
+    "revision": "018b9458411a75acfe82",
+    "url": "/color-app/static/css/main.b71dfdf7.chunk.css"
+  },
+  {
+    "revision": "442fe8de46dd5ac53fb9",
+    "url": "/color-app/static/js/2.b9b66919.chunk.js"
+  },
+  {
+    "revision": "018b9458411a75acfe82",
+    "url": "/color-app/static/js/main.e30c77d9.chunk.js"
+  },
+  {
+    "revision": "1975251fbc29e1d70f1e",
+    "url": "/color-app/static/js/runtime~main.6ad3c9d6.js"
+  },
+  {
+    "revision": "0f691328d694ef2d63b246b964c4be9c",
+    "url": "/color-app/static/media/bg.0f691328.svg"
+  }
+]);
